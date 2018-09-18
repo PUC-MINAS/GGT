cdae39804e83d0b0c6d434b0905a716975dc826a
raviassis <ravi.g.assis@gmail.com>
2018-09-18 05:39:21 -0300

    Premios
    migration table premios
    model Premio

---
+++ GGT/database/migrations/2018_09_18_082958_create_premios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePremiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('premios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->integer('valor');
+            $table->integer('vagas');
+            $table->dateTime('data_expirar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('premios');
+    }
+}
