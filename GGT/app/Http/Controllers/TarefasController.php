bd4a00885a45cce12fbde3256e8e23df19674469
raviassis <ravi.g.assis@gmail.com>
2018-09-18 05:27:35 -0300

    Tarefas e Usuários
    Melhorias interface Tarefas
    Alteração controller Tarefa
    Alteração Migration table tarefas
    Criação migration table usuarios
    Criação model usuario

--- GGT/app/Http/Controllers/TarefasController.php
+++ GGT/app/Http/Controllers/TarefasController.php
@@ -23,12 +23,13 @@ class TarefasController extends Controller
 
     public function store(Request $request){
     	
+        //dd($request->input('data_limite'));
     	$tarefa = new Tarefa();
     	$tarefa->titulo = $request->input('titulo');
     	$tarefa->descricao = $request->input('descricao');
     	$tarefa->recompensa = $request->input('recompensa');
-    	$tarefa->data_entrega = DateTime::createFromFormat('Y-m-d H:i:s', '2018-09-24 00:00:00');//$request->input('data_entrega');
-    	//dd($tarefa);
+    	$tarefa->data_limite = DateTime::createFromFormat('Y-m-d H:i:s', $request->input('data_limite') . ' 23:59:59');
+    	//dd($tarefa); debug
 
     	$tarefa->save();
 
