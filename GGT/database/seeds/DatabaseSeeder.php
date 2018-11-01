53afa2e66d5429f6354208a194892361af4d9071
Andre Chacham <andre.chacham1@gmail.com>
2018-11-01 09:57:51 -0300

    Correcao de erro no seed

--- GGT/database/seeds/DatabaseSeeder.php
+++ GGT/database/seeds/DatabaseSeeder.php
@@ -28,9 +28,9 @@ class DatabaseSeeder extends Seeder
          ]);
 
          DB::table('users')->insert([
-           ['nome' => 'Diretor Executivo','email' => 'diretorExecutivo@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '1', 'setore_id' => '4'],
-           ['nome' => 'Diretor','email' => 'diretor@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '2', 'setore_id' => '1'],
-           ['nome' => 'Trainee','email' => 'trainee@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '3', 'setore_id' => '1']
+           ['nome' => 'Diretor Executivo','email' => 'diretorExecutivo@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '1', 'setores_id' => '4'],
+           ['nome' => 'Diretor','email' => 'diretor@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '2', 'setores_id' => '1'],
+           ['nome' => 'Trainee','email' => 'trainee@gmail.com', 'senha' => '123456', 'tipos_usuarios_id' => '3', 'setores_id' => '1']
          ]);
      }
 }
